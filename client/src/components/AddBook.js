8176ea64b4928ad95fd1f6f2bed2901899e70199
Kostas Kourouklidis <kokoyroy@gmail.com>
2021-06-22 16:50:57 +0300

    progress until now

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
-import { getAuthorsQuery } from '../queries/Queries';
+import { getAuthorsQuery, addBookMutation } from '../queries/Queries';
 import { graphql } from 'react-apollo';
+import { compose } from 'redux'
 import style from './AddBook.module.css';
 
 export class AddBook extends Component {
     state = {
-        bookname: '',
+        name: '',
         genre: '',
         authorId: ''
     }
 
     displayOptions() {
-        const data = this.props.data;
+        const data = this.props.getAuthorsQuery;
         return data.loading ? <option>loading</option> :
             data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>);
     }
 
+    addBook() {
+
+    }
+
     submit(e) {
         e.preventDefault()
-        console.log(this.state)
+        console.log(this.props)
+        this.props.addBookMutation({
+            variables: {
+                name: this.state.name,
+                genre: this.state.genre,
+                authorId: this.state.authorId
+            }
+        })
+        console.log(this.props)
     }
 
     render() {
+        // console.log(this.props)
         return (
             <form className={style.form} onSubmit={(e) => this.submit(e)}>
                 <div className="field">
                     <label>Book name</label>
-                    <input onChange={(e) => this.setState({ bookname: e.target.value })} type="text" />
+                    <input onChange={(e) => this.setState({ name: e.target.value })} type="text" />
                 </div>
                 <div className="field">
                     <label>Genre</label>
@@ -47,4 +61,7 @@ export class AddBook extends Component {
 
 
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+    graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -9,6 +9,7 @@ export class Booklist extends Component {
 
   displayBooks() {
     let data = this.props.data;
+    console.log(this.props)
     return data.loading ?
       <h2>loading...</h2> :
       (data.books.map(
